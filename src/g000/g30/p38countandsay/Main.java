865f4b1617c2064d2d21e259aaf7d22c4e37082e
Mario Macías <mario@midokura.com>
2017-10-11 12:42:37 +0200

    Count and say

---
+++ src/g000/g30/p38countandsay/Main.java
@@ -0,0 +1,9 @@
+package g000.g30.p38countandsay;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("new Solution().countAndSay(4) = " + new Solution().countAndSay(4));
+
+	}
+}
---
+++ src/g000/g30/p38countandsay/Solution.java
@@ -0,0 +1,49 @@
+package g000.g30.p38countandsay;
+
+public class Solution {
+
+    private static final int ARR_SIZE = 100000;
+
+    public String countAndSay(int n) {
+        char[] src = new char[ARR_SIZE];
+        char[] dst = new char[ARR_SIZE];
+
+        src[0] = '1';
+        int lastLength = 1;
+        for (int i = 2 ; i <= n ; i++) {
+            lastLength = countAndSayLong(src, lastLength, dst);
+            char[] tmp = src;
+            src = dst;
+            dst = tmp;
+        }
+
+        return String.valueOf(src, 0, lastLength);
+    }
+
+    /**
+     * @return length of the dst char array
+     */
+    public int countAndSayLong(char[] src, int srcLength, char[] dst) {
+        char last = src[0];
+        int srcIndex = 1;
+        int dstLength = 0;
+        int lastCount = 1;
+
+        for (int i = 1 ; i < srcLength ; i++) {
+            if (src[i] == last) {
+                lastCount++;
+            } else {
+                dst[dstLength++] = (char) ('0' + lastCount);
+                dst[dstLength++] = (char) ('0' + last);
+                last = src[i];
+                lastCount = 1;
+            }
+        }
+
+        dst[dstLength++] = (char) ('0' + lastCount);
+        dst[dstLength++] = (char) ('0' + last);
+
+        return dstLength;
+    }
+
+}
