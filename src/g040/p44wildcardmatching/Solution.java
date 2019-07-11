a0ca6609e34e9e77ceda7b3c5906c66bb3a88bc3
Mario Macias <mmacias@newrelic.com>
2019-07-11 15:17:34 +0200

    clearer wildcard matching

--- src/g040/p44wildcardmatching/Solution.java
+++ src/g040/p44wildcardmatching/Solution.java
@@ -2,25 +2,19 @@ package g040.p44wildcardmatching;
 
 /**
  * DP approach, using a matrix that represents an inverse path from the success
- * path[M][N] to the origin (path[0][0])
- *
+ * path[M][N] to the origin (path[1][1])
+ * <p>
  * Each position of the matrix is built as a function of the neighbors (if you
  * can reach such position from any neighbor that allows stepping in diagonal
  * or in any way) and the pattern matching of the corresponding input and
- * pattern chars.
+ * pattern chars. if path[x][y] == true, it means that there is any way to
+ * reach this state
  */
 public class Solution {
-
-    private static final int NOWAY = 0;
-    private static final int DIAGONAL = 1;
-    private static final int ANYWAY = 2;
-
-    private int[][] path;
 
     public boolean isMatch(String input, String pattern) {
         char[] in = input.toCharArray();
         char[] pt = pattern.toCharArray();
-        path = new int[in.length][pt.length];
         return matches(in, pt);
     }
 
@@ -37,49 +31,42 @@ public class Solution {
             return false;
         }
 
-        final int M = input.length - 1;
-        final int N = pattern.length - 1;
+        // we give more dimension to not check top-left boundaries
+        boolean[][] path = new boolean[input.length + 1][pattern.length + 1];
 
-        // bottom right of the matrix
-        path[M][N] = direction(input[M], pattern[N]);
 
-        // right edge of the matrix
-        for (int r = M - 1; r >= 0 && path[r + 1][N] == ANYWAY; r--) {
-            path[r][N] = direction(input[r], pattern[N]);
-        }
+        final int M = input.length;
+        final int N = pattern.length;
 
-        // bottom edge of the matrix
-        for (int c = N - 1; c >= 0 && path[M][c + 1] == ANYWAY; c--) {
-            path[M][c] = direction(input[M], pattern[c]);
-        }
+        path[M][N] = true;
 
-        for (int r = M - 1; r >= 0; r--) {
-            for (int c = N - 1; c >= 0; c--) {
-                if (path[r + 1][c] == ANYWAY
-                    || path[r + 1][c + 1] == DIAGONAL
-                    || path[r][c + 1] == ANYWAY) {
-                    path[r][c] = direction(input[r], pattern[c]);
+        for (int r = M; r > 0; r--) {
+            for (int c = N; c > 0; c--) {
+                if (path[r][c]) {
+                    switch (pattern[c - 1]) {
+                        case '?':
+                            if (r >= 1) {
+                                path[r - 1][c - 1] = true;
+                            }
+                            break;
+                        case '*':
+                            path[r - 1][c - 1] = true;
+                            path[r - 1][c] = true;
+                            path[r][c - 1] = true;
+                            break;
+                        default:
+                            if (input[r - 1] == pattern[c - 1]) {
+                                path[r - 1][c - 1] = true;
+                            }
+                            break;
+                    }
                 }
             }
         }
 
         // ignore leading asterisks
         int c;
-        for (c = 0 ; path[0][c] == NOWAY && pattern[c] == '*'; c++);
-        return path[0][c] != NOWAY;
-    }
-
-    int direction(char input, char pattern) {
-        switch (pattern) {
-            case '?':
-                return DIAGONAL;
-            case '*':
-                return ANYWAY;
-            default:
-                if (pattern == input) {
-                    return DIAGONAL;
-                }
-        }
-        return NOWAY;
+        for (c = 0 ; !path[0][c] && pattern[c] == '*'; c++);
+        return path[0][c];
     }
 }
